bb4b0f5f6267070ffe6039f320033b6f242210f0
declanrjb <declanrjb@gmail.com>
2023-07-26 13:47:58 -0400

    Curvature test

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function createLine(source, target){
+  var dx = target[0] - source[0],
+      dy = target[1] - source[1],
+      dr = Math.sqrt(dx * dx + dy * dy)/1;
+  
+  return "M" +
+    source[0] + "," +
+    source[1] + "A" +
+    dr + "," + dr + " 0 0,1 " +
+    target[0] + "," +
+    target[1];
+}
+
+function getPointPos(point){
+  var pos = $(point).position();
+  pos.left += point.clientWidth/2;
+  return pos;
+}
+
 var path1 = document.querySelector(".p1"),
     path2 = document.querySelector(".p2");
 
@@ -8,28 +27,5 @@ var c1 = document.querySelector(".c1"),
     c3 = document.querySelector(".c3"),
     c3Pos = getPointPos(c3);
 
-
-function getPointPos(point){
-  var pos = $(point).position();
-  pos.left += point.clientWidth/2;
-  return pos;
-}
-
-
-
 path1.setAttribute('d', createLine([c1Pos.left, c1Pos.top], [c2Pos.left, c2Pos.top]));
 path2.setAttribute('d', createLine([c1Pos.left, c1Pos.top], [c3Pos.left, c3Pos.top]));
-
-
-function createLine(source, target){
-  var dx = target[0] - source[0],
-      dy = target[1] - source[1],
-      dr = Math.sqrt(dx * dx + dy * dy)/1.5;
-  
-  return "M" +
-    source[0] + "," +
-    source[1] + "A" +
-    dr + "," + dr + " 0 0,1 " +
-    target[0] + "," +
-    target[1];
-}
