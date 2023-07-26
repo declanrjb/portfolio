4281a527b08362489f0d8f5db4251679fca2fb38
declanrjb <declanrjb@gmail.com>
2023-07-26 14:09:29 -0400

    bottom right test

--- script.js
+++ script.js
@@ -27,7 +27,5 @@ var c1 = document.querySelector(".c1"),
     c3 = document.querySelector(".c3"),
     c3Pos = getPointPos(c3);
 
-print(c1Pos)
-
-path1.setAttribute('d', createLine([c1Pos.left, c1Pos.top], [c2Pos.left, c2Pos.top]));
+path1.setAttribute('d', createLine([c1Pos.right, c1Pos.bottom], [c2Pos.right, c2Pos.bottom]));
 path2.setAttribute('d', createLine([c1Pos.left, c1Pos.top], [c3Pos.left, c3Pos.top]));
